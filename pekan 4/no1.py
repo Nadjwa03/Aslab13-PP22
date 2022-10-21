002e61579a8feedd38a35f3fb99e7b5abbdd4625
Nadjwa03 <113653845+Nadjwa03@users.noreply.github.com>
2022-10-21 09:11:55 +0800

    Add files via upload

--- pekan 4/no1.py
+++ pekan 4/no1.py
@@ -1,13 +1,10 @@
-# 1. Buatlah sebuah program yang menerima inputan sebuah bilangan bulat kemudian mengembalikan nilai faktorial dari bilangan tersebut menggunakan recursive function.
+n = int(input('Masukkan nilai n: '))
 
+def hitung_faktorial (n):
+  if n > 2:
+    return n * hitung_faktorial(n - 1)
 
-def faktorial(angka, total):
-  if angka == 0:
-    return
-  
-  total = total * angka
-  
-  faktorial(angka-1, total)
-  print(total)
-  
-faktorial(8,1)
+  return 2
+
+faktorial = hitung_faktorial(n)
+print(f'{n}! = {faktorial}')
