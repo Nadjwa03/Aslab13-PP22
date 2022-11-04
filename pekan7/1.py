8d8975e5cc461c999d1a4548b6d4b35cdc72de7f
Nadjwa03 <113653845+Nadjwa03@users.noreply.github.com>
2022-11-04 09:56:12 +0800

    Add files via upload

---
+++ pekan7/1.py
@@ -0,0 +1,17 @@
+# import re
+# s= input()
+# if len(s)==45 :
+#     pola1= r"(\A([A-Za-z02468]){40})[\s13579]{5}"
+#     result1= re.match(pola1, s)
+#     if result1 :
+#         print("true")
+#     else :
+#         print("false")
+# else :
+#     print("false")
+
+bolean = None
+if bolean :
+    print("benar")
+else :
+    print ("salah")
